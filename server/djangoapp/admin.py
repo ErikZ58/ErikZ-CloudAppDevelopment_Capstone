835da262bbec519834a93d0327d762ffcc744f48
ErikZ58 <rserze0815@gmail.com>
2022-08-23 11:47:12 +0000

    models + admin and inline coding finished but not yet tested

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -7,9 +7,19 @@ admin.site.register(CarMake)
 admin.site.register(CarModel)
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel 
+    extra = 5
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['type', 'name', 'dealer_id', 'year']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['type', 'name', 'dealer_id', 'year']
+    inlines = [CarModelInline]
 
 # Register models here
+admin.site.register(CarMakeAdmin)
+admin.site.register(CarModelAdmin)
