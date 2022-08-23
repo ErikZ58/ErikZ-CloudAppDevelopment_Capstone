e3b2f6d91e1175328c9e8f186b4cda8b30e93766
ErikZ58 <rserze0815@gmail.com>
2022-08-23 16:26:39 +0000

    first bugs fixed, still open errors

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -3,23 +3,23 @@ from .models import CarMake, CarModel
 
 
 # Register your models here.
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+#admin.site.register(CarMake)
+#admin.site.register(CarModel)
 
 # CarModelInline class
 class CarModelInline(admin.StackedInline):
     model = CarModel 
     extra = 5
 
-# CarModelAdmin class
+#CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
-    fields = ['type', 'name', 'dealer_id', 'year']
+    fields = ['car_type', 'model_name']
 
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
-    fields = ['type', 'name', 'dealer_id', 'year']
+    fields = ['description', 'maker_name']
     inlines = [CarModelInline]
 
 # Register models here
-admin.site.register(CarMakeAdmin)
-admin.site.register(CarModelAdmin)
+admin.site.register(CarMake,CarMakeAdmin)
+admin.site.register(CarModel,CarModelAdmin)
